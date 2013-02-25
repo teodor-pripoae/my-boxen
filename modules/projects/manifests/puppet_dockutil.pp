add85d4075744d710e2d10a24febc12a1d404bf8
Graham Gilbert <graham@grahamgilbert.com>
2013-02-25 15:52:12 +0000

    Be more careful when copying and pasting!

--- modules/projects/manifests/puppet_dockutil.pp
+++ modules/projects/manifests/puppet_dockutil.pp
@@ -1,11 +1,11 @@
-class projects::rcoleman_mac_profiles_handler (
+class projects::puppet_dockutil (
 	$my_homedir   = $people::grahamgilbert::params::my_homedir,
   	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
   	$my_username  = $people::grahamgilbert::params::my_username
 	){
 	
-	boxen::project { 'rcoleman_mac_profiles_handler':
-		dir		=>	"${my_sourcedir}/Mine/rcoleman-mac_profiles_handler",
-		source	=>	'grahamgilbert/rcoleman-mac_profiles_handler',
+	boxen::project { 'puppet_dockutil':
+		dir		=>	"${my_sourcedir}/Mine/puppet-dockutil",
+		source	=>	'grahamgilbert/puppet-dockutil',
 	}
 }
--- modules/projects/manifests/rcoleman_mac_profiles_handler.pp
+++ modules/projects/manifests/rcoleman_mac_profiles_handler.pp
@@ -1,11 +1,11 @@
-class projects::puppet_dockutil (
+class projects::rcoleman_mac_profiles_handler (
 	$my_homedir   = $people::grahamgilbert::params::my_homedir,
   	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
   	$my_username  = $people::grahamgilbert::params::my_username
 	){
 	
-	boxen::project { 'puppet_dockutil':
-		dir		=>	"${my_sourcedir}/Mine/puppet-dockutil",
-		source	=>	'grahamgilbert/puppet-dockutil',
+	boxen::project { 'rocleman_mac_profiles_handler':
+		dir		=>	"${my_sourcedir}/Mine/rcoleman-mac_profiles_handler",
+		source	=>	'grahamgilbert/rcoleman-mac_profiles_handler',
 	}
 }
