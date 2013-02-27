0dde11a16890782b0510b8c20441d0d8b74bb3f6
Graham Gilbert <graham@grahamgilbert.com>
2013-02-27 12:27:56 +0000

    OOPS

--- modules/projects/manifests/puppet.pp
+++ modules/projects/manifests/puppet.pp
@@ -1,11 +1,23 @@
-class projects::puppet_bootstrap (
+class projects::puppet (
 	$my_homedir   = $people::grahamgilbert::params::my_homedir,
   	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
   	$my_username  = $people::grahamgilbert::params::my_username
 	){
 	
-	boxen::project { 'puppet_bootstrap':
-		dir		=>	"${my_sourcedir}/Work/puppet-bootstrap",
-		source	=>	'pebbleit/puppet-bootstrap',
+	boxen::project { 'puppet':
+		dir		=>	"${my_sourcedir}/Others/puppet",
+		source	=>	'puppetlabs/puppet',
+	}
+	
+	boxen::project { 'facter':
+		dir		=>	"${my_sourcedir}/Others/facter",
+		source	=>	'puppetlabs/facter',
+	}
+	
+	file { '/bin/envpuppet':
+	  ensure  => link,
+	  mode    => '0755',
+	  target  => "${people::grahamgilbert::params::my_sourcedir}/Others/puppet/ext/envpuppet",
+	  require => Boxen::Project["puppet"],
 	}
 }
