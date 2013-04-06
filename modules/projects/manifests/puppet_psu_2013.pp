17cbd95744a2b2efff8c27bbabc047117f93eedb
Graham Gilbert <graham@grahamgilbert.com>
2013-04-06 08:58:50 +0100

    More damn typos

--- modules/projects/manifests/puppet_psu_2013.pp
+++ modules/projects/manifests/puppet_psu_2013.pp
@@ -1,11 +1,11 @@
-class projects::psu_puppet_demo (
+class projects::puppet_psu_2013 (
 	$my_homedir   = $people::grahamgilbert::params::my_homedir,
   	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
   	$my_username  = $people::grahamgilbert::params::my_username
 	){
 	
-	boxen::project { 'psu_puppet_2013':
-		dir		=>	"${my_sourcedir}/Mine/psu_puppet_2013",
-		source	=>	'grahamgilbert/psu_puppet_2013',
+	boxen::project { 'puppet_psu_2013':
+		dir		=>	"${my_sourcedir}/Mine/puppet_psu_2013",
+		source	=>	'grahamgilbert/puppet_psu_2013',
 	}
 }
