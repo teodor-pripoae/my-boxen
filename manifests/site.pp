31033f92ed675148b70ae55c1819ca9377b773d2
Graham Gilbert <graham@grahamgilbert.com>
2013-04-07 10:16:51 +0100

    Oops

--- manifests/site.pp
+++ manifests/site.pp
@@ -63,10 +63,10 @@ node default {
   #}
 
   # node versions
-  include nodejs::0_4
-  include nodejs::0_6
-  include nodejs::0_8
-  include nodejs::0_10
+  include nodejs::v0_4
+  include nodejs::v0_6
+  include nodejs::v0_8
+  include nodejs::v0_10
 
   # default ruby versions
   include ruby::1_8_7
