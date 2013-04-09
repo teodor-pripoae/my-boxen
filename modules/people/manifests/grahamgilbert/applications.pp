c6970c940d558e249353b60ff054d16beb973621
Graham Gilbert <graham@grahamgilbert.com>
2013-04-09 07:27:11 +0100

    Try to learn how to deal with the issue first - keep this in reserve

--- modules/people/manifests/grahamgilbert/applications.pp
+++ modules/people/manifests/grahamgilbert/applications.pp
@@ -97,8 +97,8 @@ class people::grahamgilbert::applications {
     	provider => appdmg,
     }
 	
-	package {'KeyRemap4MacBook':
-		source   => 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg',
-		provider => pkgdmg,
-	}
+	# package {'KeyRemap4MacBook':
+	# 	source   => 'http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.0.0.dmg',
+	# 	provider => pkgdmg,
+	# }
 }
