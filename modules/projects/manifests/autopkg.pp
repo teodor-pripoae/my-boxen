7d2d57ee2e6092f42024fabeb90dc5bdafbde22c
Graham Gilbert <graham@grahamgilbert.com>
2013-09-03 17:21:10 +0100

    Autopkg setup

---
+++ modules/projects/manifests/autopkg.pp
@@ -0,0 +1,22 @@
+class projects::autopkg (
+	$my_homedir   = $people::grahamgilbert::params::my_homedir,
+  	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
+  	$my_username  = $people::grahamgilbert::params::my_username
+	){
+	
+	
+	
+	boxen::project { 'autopkg':
+		dir		=>	"${my_sourcedir}/Others/autopkg",
+		source	=>	'autopkg/autopkg',
+		#ruby	=>	'1.9.3-p392',
+	}
+    
+    # config for AutoPkg
+	boxen::osx_defaults { 'autopkg Munki repository':
+		  ensure => present,
+		  domain => 'com.github.autopkg',
+		  key    => 'MUNKI_REPO',
+		  value  => '/Volumes/munki.pebbleit.com',
+	}
+}
