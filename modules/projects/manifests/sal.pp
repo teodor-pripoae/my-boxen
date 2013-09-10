3394704d86fc404d7f40d856d658df08aab91cad
Graham Gilbert <graham@grahamgilbert.com>
2013-09-10 18:03:31 +0100

    Sal

---
+++ modules/projects/manifests/sal.pp
@@ -0,0 +1,14 @@
+class projects::sal (
+	$my_homedir   = $people::grahamgilbert::params::my_homedir,
+  	$my_sourcedir = $people::grahamgilbert::params::my_sourcedir,
+  	$my_username  = $people::grahamgilbert::params::my_username
+	){
+	
+	
+	
+	boxen::project { 'sal':
+		dir		=>	"${my_sourcedir}/Mine/sal",
+		source	=>	'grahamgilbert/sal',
+		#ruby	=>	'1.9.3-p392',
+	}
+}
