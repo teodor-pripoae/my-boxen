86d89f868ad21619992f5f43981c07395ff3acc0
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-04-25 01:43:28 +0300

    Add more packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,16 +56,30 @@ node default {
   #include dnsmasq
   include git
   include hub
+  include java
   include ruby
-  include osx
-  include iterm2
+  include phantomjs
+  include osx::global::disable_key_press_and_hold
+  include osx::global::disable_autocorrect
+  include iterm2::stable
   include chrome
+  include firefox
+  include caffeine
+  include appcleaner
+  include googledrive
   include dropbox
   include alfred
+  include teamviewer
+  include virtualbox
   include vagrant
   include textmate
+  include emacs_for_mac
   include postgresapp
-  include mongodb
+  # include mongodb
+  include redis
+  include mysql
+  include zsh
+  include ohmyzsh
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -83,17 +97,111 @@ node default {
   # ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
 
+  ruby::version { 'jruby-1.7.9': }
+
   # common, useful packages
   package {
     [
-      'ack',
-      'findutils',
-      'gnu-tar'
+     'ack',
+     'findutils',
+     'gnu-tar',
+     'the_silver_searcher',
+     'mongodb',
+     'memcached'
     ]:
   }
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
     target => $boxen::config::repodir
+  }
+
+  git::config::global { 'user.email':
+    value => 'teodor.pripoae@gmail.com'
+  }
+
+  git::config::global { 'user.name':
+    value => 'Teodor Pripoae'
+  }
+
+  #Generate Github ssh key if not present
+  $ssh_rsa_key = "/Users/toni/.ssh/id_rsa.pub"
+  exec {'Generate SSH key for Github':
+    command => "ssh-keygen -f ${ssh_rsa_key} -t rsa -N ''",
+    unless  => "test -e #{ssh_rsa_key}"
+  }
+
+  $home_path = "/Users/toni"
+  $emacs_dir = "${home_path}/.emacs.d"
+  $dotfiles_dir = "${home_path}/.dotfiles"
+
+  repository { '.emacs.d':
+    source => 'https://github.com/teodor-pripoae/dotemacs.git',
+    path => $emacs_dir,
+    provider => 'git'
+  }
+
+  repository { '.dotfiles':
+    source => "https://github.com/teodor-pripoae/dotfiles.git",
+    path => $dotfiles_dir,
+    provider => 'git'
+  }
+
+  repository { 'scmbreeze':
+    source => 'https://github.com/ndbroadbent/scm_breeze.git',
+    path   => "${home_path}/.scm_breeze",
+    provider => 'git'
+  }
+
+  exec { 'install scmbreeze':
+    command => "${home_path}/.scm_breeze/install.sh",
+    creates => "${home_path}/.scmbrc",
+    refreshonly => true,
+    subscribe => Repository['scmbreeze']
+  }
+
+  exec { 'copy zshrc':
+    command => "ln -s ${home_path}/.dotfiles/zsh/zshrc ${home_path}/.zshrc",
+    creates => "${home_path}/.zshrc",
+    subscribe => Repository['.dotfiles']
+  }
+
+  exec { 'copy theme':
+    command => "ln -s ${home_path}/.dotfiles/zsh/ersoft.zsh-theme ${home_path}/.oh-my-zsh/themes/ersoft.zsh-theme",
+    creates => "${home_path}/.oh-my-zsh/themes/ersoft.zsh-theme",
+    subscribe => Repository['.dotfiles']
+  }
+
+  file { "${home_path}/.zshrc":
+    ensure => "link",
+    target => "${home_path}/.dotfiles/zsh/zshrc",
+    require => [Repository["robbyrussell_oh-my-zsh"],
+                Repository[".dotfiles"]]
+  }
+
+  file { "${home_path}/.oh-my-zsh/themes/ersoft.zsh-theme":
+    ensure => "link",
+    target => "${home_path}/.dotfiles/zsh/ersoft.zsh-theme",
+    require => [Repository["robbyrussell_oh-my-zsh"],
+                Repository[".dotfiles"]]
+  }
+
+  file { "${home_path}/.aliases.sh":
+    ensure => "link",
+    target => "${home_path}/.dotfiles/zsh/aliases.sh",
+    require => Repository[".dotfiles"]
+  }
+
+  # Erlang /Elixir
+
+  repository {
+    "exenv":
+      source => "https://github.com/mururu/exenv.git",
+      path => "${home_path}/.exenv",
+      provider => "git";
+    "exenv-build":
+      source => "https://github.com/mururu/elixir-build.git",
+      path => "${home_path}/.exenv/plugins/elixir-build",
+      provider => "git"
   }
 }
